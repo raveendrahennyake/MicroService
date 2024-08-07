b8557abe8bf5833a700cf34545716bb97afdb1de
Raveendra Hennyake <raveendralakshan5@gmail.com>
2024-08-07 18:12:33 +0530

    save method build
    save method build and add two conditions inventory micoservice quantity and product microservice forsale

--- ABCCompaney/orders/src/main/java/com/example/demo/Service/OrderService.java
+++ ABCCompaney/orders/src/main/java/com/example/demo/Service/OrderService.java
@@ -1,28 +1,36 @@
 package com.example.demo.Service;
 import com.example.demo.D.T.O.InventoryDTO;
 import com.example.demo.D.T.O.OrderDTO;
+import com.example.demo.D.T.O.ProductDTO;
 import com.example.demo.Model.Orders;
 import com.example.demo.Repository.OrderRepo;
 import com.example.demo.common.ErrorResponce;
 import com.example.demo.common.OrderResponce;
 import com.example.demo.common.SucessOrderResponce;
+import com.example.demo.config.WebClientConfig;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import java.util.List;
 @Service
 public class OrderService {
     @Autowired
-    private OrderRepo orderRepo;
-
+    private final OrderRepo orderRepo;
     @Autowired
-    private ModelMapper modelMapper;
-    private final WebClient webClient;
+    private final ModelMapper modelMapper;
+    @Autowired
+    @Qualifier("inventoryWebClient")
+    private final WebClient inventoryWebClient;
+    @Autowired
+    @Qualifier("ProductWebClient")
+    private final WebClient ProductWebClient;
 
-    public OrderService(WebClient.Builder webClientBuilder,OrderRepo orderRepo,ModelMapper modelMapper) {
-       this.webClient=webClientBuilder.baseUrl("http://localhost:8081/api/v1").build();
+    public OrderService(WebClient inventoryWebClient,WebClient ProductWebClient, OrderRepo orderRepo,ModelMapper modelMapper) {
+        this.ProductWebClient=ProductWebClient;
+        this.inventoryWebClient=inventoryWebClient;
         this.orderRepo=orderRepo;
         this.modelMapper=modelMapper;
     }
@@ -33,19 +41,31 @@ public class OrderService {
     public OrderResponce saveOrder(OrderDTO OrderDTO) {
         Integer itemId=OrderDTO.getItemId();
         try {
-            InventoryDTO inventoryResponce=webClient.get()
+            InventoryDTO inventoryResponce=inventoryWebClient.get()
                     .uri(uriBuilder ->uriBuilder.path("/item/{itemId}").build(itemId))
                     .retrieve()
                     .bodyToMono(InventoryDTO.class)
                     .block();
             assert inventoryResponce != null;
+
+            Integer productId=inventoryResponce.getProductId();
+            ProductDTO productResponce=ProductWebClient.get()
+                    .uri(uriBuilder ->uriBuilder.path("/product/{productId}").build(productId))
+                    .retrieve()
+                    .bodyToMono(ProductDTO.class)
+                    .block();
+            assert productResponce!=null;
+
             if (inventoryResponce.getQuantity()>0){
-                orderRepo.save(modelMapper.map(OrderDTO, Orders.class));
-                return new SucessOrderResponce(OrderDTO) ;
+                if(productResponce.getForSale()==1){
+                    orderRepo.save(modelMapper.map(OrderDTO, Orders.class));
+                    return new SucessOrderResponce(OrderDTO) ;
+                }else {
+                    return new ErrorResponce("this item not for sale");
+                }
 
             }else{
                 return new ErrorResponce("this item not availabel ");
-
             }
         }
         catch (Exception e){
--- ABCCompaney/orders/src/main/java/com/example/demo/config/WebClientConfig.java
+++ ABCCompaney/orders/src/main/java/com/example/demo/config/WebClientConfig.java
@@ -1,4 +1,5 @@
 package com.example.demo.config;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,5 +9,15 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient (){
         return WebClient.builder().build();
+    }
+    @Bean
+    @Qualifier("inventoryWebClient")
+    public WebClient inventoryWebClient (){
+        return WebClient.builder().baseUrl("http://localhost:8081/api/v1").build();
+    }
+    @Bean
+    @Qualifier("ProductWebClient")
+    public WebClient ProductWebClient (){
+        return WebClient.builder().baseUrl("http://localhost:8082/api/v1").build();
     }
 }
